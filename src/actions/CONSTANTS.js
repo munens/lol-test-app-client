52d33d2d8c465246c59b27a36113c232021e22f6
munens <kaumbuthom@yahoo.com>
2018-02-02 05:00:45 -0800

    submitting production app

--- src/actions/CONSTANTS.js
+++ src/actions/CONSTANTS.js
@@ -1,5 +1,5 @@
-export const API_URL = 'http://localhost:4000';
-//export const API_URL = 'https://damp-caverns-64688.herokuapp.com';
+//export const API_URL = 'http://localhost:4000';
+export const API_URL = 'https://damp-caverns-64688.herokuapp.com';
 
 export const GET_SUMMONER = 'GET_SUMMONER';
 export const GET_MATCH_HISTORY_DATA = 'GET_MATCH_HISTORY_DATA';
